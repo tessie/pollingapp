2d7cc29d1f2920ad53c42afaf7a1ea6f0cce40c1
tessie <tessyjoseph1992@gmail.com>
2014-07-08 11:46:39 +0530

    PyFlakes and Pep8 verfied

--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
 class Poll(models.Model):
     question = models.TextField(max_length=300)
     pub_date = models.DateField()
@@ -10,8 +10,8 @@ class Poll(models.Model):
 
 
 class Choice1(models.Model):
-    poll     =models.ForeignKey(Poll)
-    text  =   "Yes you are the only one"
+    poll = models.ForeignKey(Poll)
+    text = "Yes you are the only one"
     votes = models.IntegerField(default=0)
 
     def __unicode__(self):
@@ -19,19 +19,18 @@ class Choice1(models.Model):
 
 
 class Choice2(models.Model):
-    poll    =models.ForeignKey(Poll)
+    poll = models.ForeignKey(Poll)
     text = "No you are not the only one"
-    votes= models.IntegerField(default=0)
+    votes = models.IntegerField(default=0)
 
     def __unicode__(self):
         return self.text
 
 
 class Comment(models.Model):
-    Poll   =models.ForeignKey(Poll)
-    comment =models.TextField(max_length=300)
+    poll = models.ForeignKey(Poll)
+    comment = models.TextField(max_length=300)
     post_date = models.DateTimeField()
 
     def __unicode__(self):
         return self.comment
-
