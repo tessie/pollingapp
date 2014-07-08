cdf302f39cc5e36a19545c36b2a734779f62bc23
tessie <tessyjoseph1992@gmail.com>
2014-07-08 09:05:19 +0530

    Initial commit

---
+++ pollingapp/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+from django.conf import settings
+
+# UNDERNEATH your urlpatterns definition, add the following two lines:
+
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'pollingapp.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+   # url(r'^polls/',include(polls.urls,namespace='polls',)),
+    url(r'^admin/', include(admin.site.urls)),
+)
+if settings.DEBUG:
+    urlpatterns += patterns(
+        'django.views.static',
+        (r'media/(?P<path>.*)',
+        'serve',
+        {'document_root': settings.MEDIA_ROOT}), )
